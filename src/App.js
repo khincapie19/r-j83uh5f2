bb80934ebd6c1783b63bd2ab6950ee838d77ae0e
Katherine <katherinehincapie19@hotmail.com>
2020-09-19 21:53:54 -0400

    good

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+
+  }
+  // updateCount(event) {
+  //   const eventKey = event.keyCode
+  //   //const counter = eventKey === 13 || eventKey > 33 ? this.state.count + 1 : this.state.count > 0 ? this.state.count - 1 : this.state.count === 0
+  //   const counter = eventKey !== 8 ? this.state.count + 1 : this.state.count > 0 ? this.state.count - 1 : this.state.count === 0
+  //   console.log(eventKey)
+  //
+  //   this.setState({
+  //     count: counter
+  //   });
+  // }
+
+  updateCount(event){
+    const count = event.target.value.length;
+    this.setState({
+      count: count
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea onChange={this.updateCount.bind(this)} rows="3"></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
